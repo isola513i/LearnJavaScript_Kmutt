bc729a6c49f7c745fc41814826e193a85c5edcde
Macbook-Isolicez <okuma530@gmail.com>
2024-08-08 11:43:02 +0700

    update

--- Week01/demo.js
+++ Week01/demo.js
@@ -1,18 +1,23 @@
 //function declaration
-function calculator(op,num1,num2){
-    return op(num1,num2);
+function calculator(op, num1, num2){
+    console.log(typeof op)
+    console.log(typeof num1)
+    console.log(typeof num2)
+    return op(num1, num2)
 }
 //function expression (arrow function)
-const sum=(num1,num2)=> {
+const sum=(num1, num2)=>{
     return num1+num2
 }
-
-const substract=(num1,num2){
+const subtract=(num1, num2)=>{
     return num1-num2
 }
-
-const randomNumber = (){
-    const randNum=Math.floor(Math.random()*10)+1
+const randomNumber = ()=>{
+    const randNum=Math.floor(Math.random()*10)+1 //1-10
     return randNum
-
 }
+
+const num1=randomNumber()
+const num2=randomNumber()
+//op is a higher order function
+console.log(calculator(sum, num1,num2))
