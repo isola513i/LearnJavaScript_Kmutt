530a3562672186fef8f25485f1cafcf157d24cee
Macbook-Isolicez <okuma530@gmail.com>
2024-08-08 11:08:50 +0700

    randomNum

--- Week01/demo.js
+++ Week01/demo.js
@@ -1,1 +1,18 @@
-console.log("Hello world")
+//function declaration
+function calculator(op,num1,num2){
+    return op(num1,num2);
+}
+//function expression (arrow function)
+const sum=(num1,num2)=> {
+    return num1+num2
+}
+
+const substract=(num1,num2){
+    return num1-num2
+}
+
+const randomNumber = (){
+    const randNum=Math.floor(Math.random()*10)+1
+    return randNum
+
+}
